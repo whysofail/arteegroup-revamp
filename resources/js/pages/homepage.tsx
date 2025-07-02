87c8a7518b6ecb251bbed4b746fa2ac7f7e77814
whysofail <adam.satria.armansyah@gmail.com>
2025-07-02 17:49:42 +0700

    feat: page preview, base blocks

--- resources/js/pages/homepage.tsx
+++ resources/js/pages/homepage.tsx
@@ -39,3 +39,22 @@ export default function Home() {
         </div>
     );
 }
+// resources/js/Pages/Homepage.tsx
+
+export default function Homepage({ seo, blocks }) {
+    return (
+        <>
+            <Head title={seo?.title}>
+                <meta name="description" content={seo?.description} />
+                {seo?.image && <meta property="og:image" content={seo.image} />}
+            </Head>
+
+            <main>
+                {/* Debug props */}
+                <pre>{JSON.stringify({ seo, blocks }, null, 2)}</pre>
+
+                {/* Render blocks */}
+            </main>
+        </>
+    );
+}
