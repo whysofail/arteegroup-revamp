8de279b87641b10363ff83acd87307b2048c40a9
Dipo Kusumo <m.alam@students.paramadina.ac.id>
2025-08-11 16:18:56 +0700

    fix: is_highlighted bug on prod

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -14,6 +14,7 @@ class Work extends Model implements HasMedia
     protected $guarded = [];
     protected $casts = [
         'blocks' => 'array',
+        'is_highlighted' => 'boolean',
     ];
 
     protected static function booted()
